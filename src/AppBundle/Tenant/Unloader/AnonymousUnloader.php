81313d0a2b1046b0f9287a6c4d251b50f72e1688
mario-digitalstate <mario@digitalstate.ca>
2018-09-07 17:10:34 -0400

    Refactor tenant loaders and unloaders

---
+++ src/AppBundle/Tenant/Unloader/AnonymousUnloader.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace AppBundle\Tenant\Unloader;
+
+use AppBundle\Entity\Anonymous;
+use Doctrine\ORM\EntityManager;
+use Ds\Component\Tenant\Entity\Tenant;
+use Ds\Component\Tenant\Loader\Unloader;
+
+/**
+ * Class AnonymousUnloader
+ */
+class AnonymousUnloader implements Unloader
+{
+    /**
+     * @var \Doctrine\ORM\EntityManager
+     */
+    protected $entityManager;
+
+    /**
+     * Constructor
+     *
+     * @param \Doctrine\ORM\EntityManager $entityManager
+     */
+    public function __construct(EntityManager $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function unload(Tenant $tenant)
+    {
+        $builder = $this->entityManager->getRepository(Anonymous::class)->createQueryBuilder('e');
+        $builder
+            ->delete()
+            ->where('e.tenant = :tenant')
+            ->setParameter('tenant', $tenant->getUuid());
+        $query = $builder->getQuery();
+        $query->execute();
+    }
+}
