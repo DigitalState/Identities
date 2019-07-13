c8b6fa5026cd6c2df2806f0c5b89424ac78b28a2
mario-digitalstate <mario@digitalstate.ca>
2019-07-13 14:00:56 -0400

    Add repositories

---
+++ api/src/Repository/AnonymousRoleRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Repository;
+
+/**
+ * Class AnonymousRoleRepository
+ */
+final class AnonymousRoleRepository extends BusinessUnitRoleRepository
+{
+}
---
+++ api/src/Repository/StaffRoleRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Repository;
+
+/**
+ * Class StaffRoleRepository
+ */
+final class StaffRoleRepository extends BusinessUnitRoleRepository
+{
+}
